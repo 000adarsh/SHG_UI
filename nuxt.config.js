b4d30921b2c9b01417b8084ebdbd48be4660a4b3
Adarsh <sanakihacker@gmail.com>
2022-02-27 17:08:55 +0530

    remove middleware  from global

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,9 +50,9 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-  router: {
-    middleware: 'authRouter',
-  },
+  // router: {
+  //   middleware: 'authRouter',
+  // },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
