5ca657ebb650e0a7c59ed778c8366a814082a949
adarsh <sanakihacker@gmail.com>
2022-04-08 21:00:42 +0530

    employee permission implemented

--- pages/employees/_employee/employeePermission.vue
+++ pages/employees/_employee/employeePermission.vue
@@ -10,14 +10,44 @@
       <h3>Employee Permissions</h3>
       <v-divider></v-divider>
     </div>
+    <v-simple-table
+      ><tbody v-if="permissions">
+        <tr v-for="(permission, i) in Object.keys(permissions)" :key="i">
+          <td>{{ permissions[permission] }}</td>
+        </tr>
+      </tbody></v-simple-table
+    >
   </div>
 </template>
 
 <script>
 import authRouter from '~/middleware/authRouter'
+import FetchService from '~/services/FetchService'
 export default {
   name: 'EmployeePermissionPage',
   middleware: authRouter,
+  data() {
+    return {
+      permissions: null,
+    }
+  },
+  created() {
+    this.getEmployeePermission()
+  },
+  methods: {
+    async getEmployeePermission() {
+      const permission = await FetchService.getEmployee({
+        employeeId: this.$route.params.employee,
+      })
+      if (permission) {
+        this.$root.$emit('showNotification', permission)
+      }
+      console.log(permission)
+      if (permission.data.status === 'success') {
+        this.permissions = permission.data.employee.permissions
+      }
+    },
+  },
 }
 </script>
 
