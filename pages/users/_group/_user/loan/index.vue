6ee6bc56777da649a38b8de2f0880b89ff88e86e
adarsh <sanakihacker@gmail.com>
2022-05-03 23:11:00 +0530

    active and inactive loan created

--- pages/users/_group/_user/loan/index.vue
+++ pages/users/_group/_user/loan/index.vue
@@ -114,28 +114,72 @@
       <h3 class="text-center">User Loans</h3>
       <v-divider></v-divider>
     </div>
-    <v-row v-if="loans.length"
-      ><v-col v-for="(loan, i) in loans" :key="i" cols="12" sm="6" lg="4"
-        ><v-card
-          outlined
-          :hover="true"
-          @click="
-            $router.push(
-              `/users/${$route.params.group}/${$route.params.user}/loan/${loan.id}?name=${$route.query.name}`
-            )
-          "
-          ><v-card-title>Loan Amount - {{ loan.amount }}</v-card-title
-          ><v-card-subtitle class="py-0"
-            >Created Date -
-            {{
-              $moment(loan.createDate).format('DD MMM YYYY hh:mm:ss a')
-            }}</v-card-subtitle
-          ><v-card-subtitle class="py-0"
-            >Creater - {{ loan.createdBy.name }}</v-card-subtitle
-          ></v-card
-        ></v-col
-      ></v-row
-    >
+    <div v-if="activeLoans.length">
+      <div class="pb-2">
+        <h2 class="text-center">Active Loans</h2>
+        <v-divider></v-divider>
+      </div>
+      <v-row
+        ><v-col
+          v-for="(loan, i) in activeLoans"
+          :key="i"
+          cols="12"
+          sm="6"
+          lg="4"
+          ><v-card
+            outlined
+            :hover="true"
+            @click="
+              $router.push(
+                `/users/${$route.params.group}/${$route.params.user}/loan/${loan.id}?name=${$route.query.name}`
+              )
+            "
+            ><v-card-title>Loan Amount - {{ loan.amount }}</v-card-title
+            ><v-card-subtitle class="py-0"
+              >Created Date -
+              {{
+                $moment(loan.createDate).format('DD MMM YYYY')
+              }}</v-card-subtitle
+            ><v-card-subtitle class="py-0"
+              >Creater - {{ loan.createdBy.name }}</v-card-subtitle
+            ></v-card
+          ></v-col
+        ></v-row
+      >
+    </div>
+    <div v-if="inactiveLoans.length">
+      <div class="pb-2">
+        <h2 class="text-center">Inactive Loans</h2>
+        <v-divider></v-divider>
+      </div>
+      <v-row
+        ><v-col
+          v-for="(loan, i) in inactiveLoans"
+          :key="i"
+          cols="12"
+          sm="6"
+          lg="4"
+          ><v-card
+            outlined
+            :hover="true"
+            @click="
+              $router.push(
+                `/users/${$route.params.group}/${$route.params.user}/loan/${loan.id}?name=${$route.query.name}`
+              )
+            "
+            ><v-card-title>Loan Amount - {{ loan.amount }}</v-card-title
+            ><v-card-subtitle class="py-0"
+              >Created Date -
+              {{
+                $moment(loan.createDate).format('DD MMM YYYY')
+              }}</v-card-subtitle
+            ><v-card-subtitle class="py-0"
+              >Creater - {{ loan.createdBy.name }}</v-card-subtitle
+            ></v-card
+          ></v-col
+        ></v-row
+      >
+    </div>
   </div>
 </template>
 
@@ -159,6 +203,18 @@ export default {
       dateDialog: false,
       activePicker: null,
     }
+  },
+  computed: {
+    activeLoans() {
+      return this.loans.filter((u) => {
+        return u.isActive
+      })
+    },
+    inactiveLoans() {
+      return this.loans.filter((u) => {
+        return !u.isActive
+      })
+    },
   },
   watch: {
     dateDialog(val) {
